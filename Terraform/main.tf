4d6615fe59ab763a514b9137c67fcde96923c4a6
RakeshKasagani <rakeshkasagani121@gmail.com>
2026-04-28 15:27:25 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -28,6 +28,14 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ 
+ingress {
+  description = "Jenkins"
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
   ingress {
     description = "SonarQube"
