691dde7c945e8fef94f32175fc19f1d225cb8883
RakeshKasagani <rakeshkasagani121@gmail.com>
2026-04-30 18:07:44 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 # ----------------------------
 # VPC
 # ----------------------------
@@ -17,6 +18,10 @@ resource "aws_vpc" "main_vpc" {
 # ----------------------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main-igw"
+  }
 }
 
 # ----------------------------
@@ -27,6 +32,10 @@ resource "aws_subnet" "subnet1" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
 }
 
 resource "aws_subnet" "subnet2" {
@@ -34,6 +43,10 @@ resource "aws_subnet" "subnet2" {
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
 }
 
 # ----------------------------
@@ -45,6 +58,12 @@ resource "aws_route_table" "rt" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "public-rt"
   }
 }
 
@@ -58,16 +77,21 @@ resource "aws_route_table_association" "a2" {
   route_table_id = aws_route_table.rt.id
 }
 
-# Key Pair (use your existing public key)
+# ----------------------------
+# Key Pair
+# ----------------------------
 resource "aws_key_pair" "mykey" {
   key_name   = "project"
   public_key = file("C:/Users/Hp/.ssh/id_ed25519.pub")
 }
 
+# ----------------------------
 # Security Group
+# ----------------------------
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_sg"
-  description = "Allow SSH and HTTP"
+  description = "Allow SSH and DevOps tools"
+  vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
     description = "SSH"
@@ -84,14 +108,14 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
-ingress {
-  description = "Jenkins"
-  from_port   = 8080
-  to_port     = 8080
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+
+  ingress {
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "SonarQube"
@@ -101,20 +125,22 @@ ingress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
-    description = "prometheus"
+  ingress {
+    description = "Prometheus"
     from_port   = 9090
     to_port     = 9090
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- ingress {
-    description = "app"
+
+  ingress {
+    description = "App"
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -124,10 +150,10 @@ ingress {
 }
 
 # ----------------------------
-# Instance 1: jenkins
+# Instance 1: Jenkins Server
 # ----------------------------
-resource "aws_instance" "Jenkins_server" {
-  ami           = "ami-08982f1c5bf93d976" # Amazon Linux 2 (us-east-1)
+resource "aws_instance" "jenkins_server" {
+  ami           = "ami-08982f1c5bf93d976"
   instance_type = "t2.large"
   key_name      = aws_key_pair.mykey.key_name
   subnet_id     = aws_subnet.subnet1.id
@@ -136,84 +162,50 @@ resource "aws_instance" "Jenkins_server" {
   associate_public_ip_address = true
 
   root_block_device {
-  volume_size = 30
-  volume_type = "gp3"
-}
+    volume_size = 30
+    volume_type = "gp3"
+  }
 
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
+              yum install -y git docker
 
-              sudo yum install git -y
-  
-              # Install Docker
+              systemctl start docker
+              systemctl enable docker
+              usermod -aG docker ec2-user
 
-              echo ">>> Installing Docker..."
-              sudo yum install -y docker
-              sudo systemctl start docker
-              sudo systemctl enable docker
-              sudo usermod -aG docker $USER
+              # Jenkins
+              wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+              rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+              yum install -y java-21-amazon-corretto jenkins
+              systemctl enable jenkins
+              systemctl start jenkins
 
-              # Install Jenkins
+              # Trivy
+              rpm -ivh https://github.com/aquasecurity/trivy/releases/latest/download/trivy_0.56.2_Linux-64bit.rpm
 
-              echo ">>> Installing Jenkins..."
+              # SonarQube
+              sysctl -w vm.max_map_count=262144
+              echo "vm.max_map_count=262144" >> /etc/sysctl.conf
 
-              sudo yum update -y
-              sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo
-              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-              sudo yum upgrade
-              sudo yum install java-21-amazon-corretto -y
-              sudo yum install jenkins -y
-              sudo systemctl enable jenkins
-              sudo systemctl start jenkins
+              docker run -d --name sonarqube \
+              -p 9000:9000 \
+              -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true \
+              --restart always sonarqube:community
 
-
-              # Install Trivy
-
-              echo ">>> Installing Trivy..."
-              sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/latest/download/trivy_0.56.2_Linux-64bit.rpm
-
-
-              # Install SonarQube (with Docker)
-
-              echo ">>> Installing SonarQube using Docker..."
-
-             # Fix required kernel setting
-             sudo sysctl -w vm.max_map_count=262144
-             echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf
-
-            # Remove old container if exists
-            sudo docker rm -f sonarqube || true
-
-            # Run SonarQube (updated version)
-            sudo docker run -d --name sonarqube \
-            -p 9000:9000 \
-            -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true \
-            --restart always \
-            sonarqube:community
-
-              # Install Node.js + npm
-
-              echo ">>> Installing Node.js & npm..."
-              curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
-              sudo yum install -y nodejs
-
-
-              # Final Info
-
-              echo ">>> Installation Completed!"
-              echo "Jenkins running on: http://<your-server-ip>:8080"
-              echo "SonarQube running on: http://<your-server-ip>:9000"
-              echo "Use 'sudo cat /var/lib/jenkins/secrets/initialAdminPassword' for Jenkins password."
+              # Node
+              curl -sL https://rpm.nodesource.com/setup_18.x | bash -
+              yum install -y nodejs
               EOF
 
   tags = {
-    Name = "jenkins-Server"
+    Name = "jenkins-server"
   }
 }
 
 # ----------------------------
-# Instance 2: k8s Server
+# Instance 2: K8s Server
 # ----------------------------
 resource "aws_instance" "k8s_server" {
   ami           = "ami-08982f1c5bf93d976"
@@ -225,49 +217,35 @@ resource "aws_instance" "k8s_server" {
   associate_public_ip_address = true
 
   root_block_device {
-  volume_size = 30
-  volume_type = "gp3"
-}
+    volume_size = 30
+    volume_type = "gp3"
+  }
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo yum update -y
-              
-              # Install dependencies
+              yum update -y
+              yum install -y curl wget git tar
 
-              echo ">>> Installing required packages..."
-              sudo yum install -y curl wget git tar
-
-              # Install kubectl
-
-              echo ">>> Installing kubectl..."
+              # kubectl
               curl -LO "https://dl.k8s.io/release/$(curl -sSL https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
               chmod +x kubectl
-              sudo mv kubectl /usr/local/bin/
-              kubectl version --client
-              curl -sSL "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz" -o eksctl.tar.gz
+              mv kubectl /usr/local/bin/
+
+              # eksctl
+              curl -sSL https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz -o eksctl.tar.gz
               tar -xzf eksctl.tar.gz
-              sudo mv eksctl /usr/local/bin/
-              eksctl version
+              mv eksctl /usr/local/bin/
 
-
-              # Install Helm
-
-              echo ">>> Installing Helm..."
+              # helm
               curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-
-              # Add Helm Repos
-
-              echo ">>> Adding Helm repositories..."
               helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
               helm repo add grafana https://grafana.github.io/helm-charts
               helm repo add argo https://argoproj.github.io/argo-helm
               helm repo update
-
               EOF
 
   tags = {
-    Name = "k8s-Server"
+    Name = "k8s-server"
   }
 }
