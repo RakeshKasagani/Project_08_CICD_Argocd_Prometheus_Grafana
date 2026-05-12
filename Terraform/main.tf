75d578c7da56103ac16cd8ce8fc51e9d6f50bc5b
RakeshKasagani <rakeshkasagani121@gmail.com>
2026-05-12 11:31:37 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,81 +3,6 @@ provider "aws" {
 }
 
 # ----------------------------
-# VPC
-# ----------------------------
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "main-vpc"
-  }
-}
-
-# ----------------------------
-# Internet Gateway
-# ----------------------------
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  tags = {
-    Name = "main-igw"
-  }
-}
-
-# ----------------------------
-# Subnets
-# ----------------------------
-resource "aws_subnet" "subnet1" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-1"
-  }
-}
-
-resource "aws_subnet" "subnet2" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-2"
-  }
-}
-
-# ----------------------------
-# Route Table
-# ----------------------------
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  depends_on = [aws_internet_gateway.igw]
-
-  tags = {
-    Name = "public-rt"
-  }
-}
-
-resource "aws_route_table_association" "a1" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.rt.id
-}
-
-resource "aws_route_table_association" "a2" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.rt.id
-}
-
-# ----------------------------
 # Key Pair
 # ----------------------------
 resource "aws_key_pair" "mykey" {
@@ -91,7 +16,6 @@ resource "aws_key_pair" "mykey" {
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_sg"
   description = "Allow SSH and DevOps tools"
-  vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
     description = "SSH"
@@ -160,9 +84,7 @@ resource "aws_instance" "jenkins_server" {
   ami                         = "ami-08982f1c5bf93d976"
   instance_type               = "t2.large"
   key_name                    = aws_key_pair.mykey.key_name
-  subnet_id                   = aws_subnet.subnet1.id
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  associate_public_ip_address = true
+  security_groups             = [aws_security_group.ec2_sg.name]
 
   root_block_device {
     volume_size = 30
@@ -237,9 +159,8 @@ resource "aws_instance" "k8s_server" {
   ami                         = "ami-08982f1c5bf93d976"
   instance_type               = "t2.xlarge"
   key_name                    = aws_key_pair.mykey.key_name
-  subnet_id                   = aws_subnet.subnet2.id
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  associate_public_ip_address = true
+  security_groups             = [aws_security_group.ec2_sg.name]
+
 
   root_block_device {
     volume_size = 30
